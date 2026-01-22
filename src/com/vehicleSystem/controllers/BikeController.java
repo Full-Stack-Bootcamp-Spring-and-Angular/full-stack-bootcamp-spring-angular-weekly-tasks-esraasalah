7264f720f0db7b597d9bb635cab254f84d4bea42
Esraa <Esraa@192.168.1.19>
2026-01-22 20:26:23 +0200

    merge files from another tasks

---
+++ src/com/vehicleSystem/controllers/BikeController.java
@@ -0,0 +1,62 @@
+package com.vehicleSystem.controllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.vehicleSystem.dao.DatabaseOperations;
+import com.vehicleSystem.models.Bike;
+
+import com.vehicleSystem.models.Vehicle;
+
+@Component
+public class BikeController implements VehicleController {
+
+     @Autowired
+     DatabaseOperations databaseOperations;
+
+     @Override
+     public void saveVehicle(Vehicle bike) {
+
+          databaseOperations.saveVehicle(bike);
+          databaseOperations.disConnectFromTheDatabase();
+
+     }
+
+     @Override
+     public void searchById(Integer id) {
+
+          databaseOperations.searchById(id);
+          databaseOperations.disConnectFromTheDatabase();
+
+     }
+
+     @Override
+     public void updateVehicle(Vehicle vehicle) {
+
+          databaseOperations.updateVehicle(vehicle);
+          databaseOperations.disConnectFromTheDatabase();
+
+     }
+
+     @Override
+     public void deleteVehicle(Integer id) {
+
+          databaseOperations.deleteVehicle(id);
+          databaseOperations.disConnectFromTheDatabase();
+
+     }
+
+     @Override
+     public List<Vehicle> getAllVehicles() {
+
+          return databaseOperations.getAllVehicles();
+
+     }
+
+}
---
+++ src/com/vehicleSystem/controllers/VehicleController.java
@@ -0,0 +1,19 @@
+package com.vehicleSystem.controllers;
+
+import java.util.List;
+
+import com.vehicleSystem.models.Vehicle;
+
+public interface VehicleController {
+
+     public void saveVehicle(Vehicle vehicle);
+
+     public void updateVehicle(Vehicle vehicle);
+
+     public void deleteVehicle(Integer id);
+
+     public void searchById(Integer id);
+
+     public List<Vehicle> getAllVehicles();
+
+}
